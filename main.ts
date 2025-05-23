dfa3f22ea1a70ac7ac281d3282e14ce53e3907e5
nietiene <niyomugaboetiene53@gmail.com>
2025-05-23 15:44:38 -0700

    first commit of typescript

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
 let name__: string = "Etiene";
 let int__ : number = 2;
+
+function greet (user: {name: string, age: number}) {
+    console.log(`Welcome ${user.name}`);
+}
 console.log(name__);
 console.log(int__);
